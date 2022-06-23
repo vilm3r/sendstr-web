46248d63806bc2fe9b1a970fbaa7ae12774a7a93
vilm3r <vilm3r@protonmail.com>
2022-06-23 09:15:20 -0500

    fix: update tailwind paths

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-      "./pages/**/*.{js,ts,jsx,tsx}",
-      "./components/**/*.{js,ts,jsx,tsx}",
-      "./views/**/*.{js,ts,jsx,tsx}",
+      "./src/pages/**/*.{js,ts,jsx,tsx}",
+      "./src/components/**/*.{js,ts,jsx,tsx}",
+      "./src/views/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
