21a18b64b0affa41eb79ea85712ce3f9c5f4b0ae
vilm3r <vilm3r@protonmail.com>
2022-06-23 19:04:28 -0500

    chore: delete commented code

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,41 +1,12 @@
 import "../styles/global.css"
 import { AppProps } from "next/app"
 import { useEffect, useState } from "react"
-// import { getRelays } from "../lib/localStorage"
-// import { NostrEventType, NostrType } from "../types"
 
 export default function App({ Component, pageProps }: AppProps) {
-  // const [events, setEvents] = useState<{ [k: string]: NostrEventType }>({})
-  // const [nostr, setNostr] = useState<NostrType>({
-  //   priv: "",
-  //   pub: "",
-  //   pool: null,
-  //   sub: null,
-  // })
   const [keys, setKeys] = useState<{
     pub: string
     priv: string
   } | null>(null)
-
-  // const updateEvents = async (pub: string, priv: string, event: NostrEventType) => {
-  //   const { decrypt } = await import("nostr-tools/nip04")
-  //   try {
-  //     const p = event.tags.find(([tag]) => tag === "p") || ["p", ""]
-  //     const pubkey = event.pubkey === pub ? p[1] : event.pubkey
-  //     const message = decrypt(priv, pubkey, event.content)
-  //     setEvents({
-  //       ...events,
-  //       ...{
-  //         [event.id]: {
-  //           ...event,
-  //           message,
-  //         },
-  //       },
-  //     })
-  //   } catch (e) {
-  //     console.warn(e)
-  //   }
-  // }
 
   useEffect(() => {
     void (async () => {
@@ -45,13 +16,6 @@ export default function App({ Component, pageProps }: AppProps) {
       setKeys({ priv, pub })
     })()
   }, [])
-
-  // const getLatestEvent = (events: Record<string, NostrEventType>) =>
-  //   Object.entries(events).reduce((acc, x) => {
-  //     if (acc === null) return x[1]
-  //     if (new Date(acc.created_at) < new Date(x[1].created_at)) return x[1]
-  //     return acc
-  //   }, null as NostrEventType | null)
 
   return <Component {...{ ...pageProps, keys }} />
 }
