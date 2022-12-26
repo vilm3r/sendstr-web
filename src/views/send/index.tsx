8f8323aab3ca40141456cf0d0e957d764161dbf6
vilm3r <vilm3r@protonmail.com>
2022-12-26 09:19:55 -0600

    fix: remove extra spaces from send pubkey input fixes #5

--- src/views/send/index.tsx
+++ src/views/send/index.tsx
@@ -43,7 +43,7 @@ const PeerInput = ({ peerKey, onChange, setShowScan }: PeerInputProps) => {
     <section className="mx-auto max-w-[40rem] p-4">
       <label>Peer pubkey:</label>
       <div className="relative">
-        <Input value={peerKey} onChange={(e) => onChange(e?.currentTarget?.value || "")} />
+        <Input value={peerKey} onChange={(e) => onChange(e?.currentTarget?.value.trim() || "")} />
         <div className="absolute right-0 top-0 h-full flex items-center">
           <IconButton className="w-10 h-10 mr-2" onClick={() => setShowScan(true)}>
             <div className="m-2">
