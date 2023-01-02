67ee28efade4d5c3929e6d6ff966dc5c0335ccee
Arne Pedersen <arne.lamo@gmail.com>
2023-01-02 11:36:29 +0100

    Add readonly to input to remove warning.

--- src/pages/settings/index.tsx
+++ src/pages/settings/index.tsx
@@ -46,7 +46,13 @@ export default function Settings() {
           <div className="flex justify-center">
             <div>
               <div className="inline-flex relative items-center cursor-pointer">
-                <input type="checkbox" value="" className="sr-only peer" checked={isDark} />
+                <input
+                  type="checkbox"
+                  value=""
+                  className="sr-only peer"
+                  checked={isDark}
+                  readOnly
+                />
                 <div className="w-11 h-6 bg-gray-300 peer-focus:outline-none peer-focus:ring-4 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-primary"></div>
               </div>
             </div>
