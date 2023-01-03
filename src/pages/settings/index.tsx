8740bf51f026079d96bf9f2730de60e18014cbd7
Arne Pedersen <arne.lamo@gmail.com>
2023-01-03 11:18:59 +0100

    Fix switch color bug, add consistent styles, add focus-visible.

--- src/pages/settings/index.tsx
+++ src/pages/settings/index.tsx
@@ -53,7 +53,7 @@ export default function Settings() {
                   checked={isDark}
                   readOnly
                 />
-                <div className="w-11 h-6 bg-gray-300 peer-focus:outline-none peer-focus:ring-4 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-primary"></div>
+                <div className="w-11 h-6 bg-gray-300 rounded-full dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-primary focus:outline-none peer-focus-visible:ring-2"></div>
               </div>
             </div>
           </div>
